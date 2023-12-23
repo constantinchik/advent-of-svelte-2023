a01b38385c225d8f0a257de1208bfe3897af355c
Constantine Chikerenda <constantine.chik@icloud.com>
2023-12-23 14:20:18 -0500

    Init with Skeleton. Update layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import '../app.postcss';
+	import {
+		AppShell,
+		AppBar,
+		AppRail,
+		AppRailAnchor
+	} from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+
+	// Floating UI for Popups
+	import {
+		computePosition,
+		autoUpdate,
+		flip,
+		shift,
+		offset,
+		arrow
+	} from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<img
+					class="w-10"
+					src="https://advent.sveltesociety.dev/_app/immutable/assets/santa-svelte.CGuQl2Dd.png"
+					alt="Svelte logo with a santa hat on top of it"
+				/>
+				<strong class="text-xl uppercase">Advent Of Svelte 2023&nbsp;</strong>
+				<span class="text-sm">(by constantinchik)</span>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/constantinchik"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<svelte:fragment slot="lead">
+				<AppRailAnchor href="/"
+					><i class="fa-solid fa-bars fa-xl"></i></AppRailAnchor
+				>
+			</svelte:fragment>
+			<AppRailAnchor href="/day1" selected={$page.url.pathname === '/day1'}>
+				<svelte:fragment slot="lead"
+					><i class="fa-solid fa-child"></i></svelte:fragment
+				>
+				<span>Day 1</span>
+			</AppRailAnchor>
+			<svelte:fragment slot="trail">
+				<AppRailAnchor
+					href="https://github.com/constantinchik"
+					target="_blank"
+					title="Account"
+					><i class="fa-brands fa-github fa-xl"></i></AppRailAnchor
+				>
+			</svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to Advent of Svelte.</h2>
+		<img
+			class="w-80"
+			src="https://advent.sveltesociety.dev/_app/immutable/assets/santa-svelte.CGuQl2Dd.png"
+			alt="Svelte logo with a santa hat on top of it"
+		/>
+		<div class="space-y-2">
+			<h3>This project was created using:</h3>
+		</div>
+		<div class="flex justify-center space-x-2">
+			<a class="btn variant-filled" href="https://svelte.dev">Svelte Kit</a>
+			<a class="btn variant-filled" href="https://www.skeleton.dev/"
+				>Skeleton UI</a
+			>
+		</div>
+	</div>
+</div>
